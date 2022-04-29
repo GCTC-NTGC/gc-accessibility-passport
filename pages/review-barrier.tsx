f32ae420dcd37d0f6fb32a78b90fa3b5772a4972
Brinda Sasikumar <Brinda.Sasikumar@gmail.com>
2022-04-29 13:26:05 -0400

    lint fix

--- pages/review-barrier.tsx
+++ pages/review-barrier.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
 import * as React from "react";
-import { FormProvider, useForm } from "react-hook-form";
 import { useIntl } from "react-intl";
 import Button from "../components/Button";
 import Layout from "../components/Layout";
