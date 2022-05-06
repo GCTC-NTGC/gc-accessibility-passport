f56eac28454366ff25b6fa55ad586287519e7f33
Yoni K <yoni.kid15@gmail.com>
2022-05-06 15:13:36 -0400

    Make styling fix and change button to link

--- pages/manager-dashboard.tsx
+++ pages/manager-dashboard.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
 import * as React from "react";
 import { useIntl } from "react-intl";
-import Button from "../components/Button";
-import { PassportCard } from "../components/formComponents/PassportCard";
+import Button, { colorMap } from "../components/Button";
+import { PassportCard } from "../components/PassportCard";
 import Layout from "../components/Layout";
 import Page, { LeftSection, RightSection } from "../components/Page";
+import Link from "next/link";
 
 const ManagerDashboard: React.FunctionComponent = () => {
   const intl = useIntl();
@@ -18,27 +18,23 @@ const ManagerDashboard: React.FunctionComponent = () => {
         defaultMessage: "My manager dashboard",
       })}
       headTitle={intl.formatMessage({
-        defaultMessage: "My manager dashboard",
+        defaultMessage: "My manager dashboard - GC Accessibility Passport",
       })}
     >
       <Page>
         <LeftSection>
           <div data-h2-margin="b(bottom, l)">
-            <p data-h2-margin="b(all, none)">
+            <p data-h2-margin="b(top, none)">
               {intl.formatMessage({
                 defaultMessage:
                   "Activate the “View passport” link beside each employee’s name to view their Passport information and take action on any solutions they have requested.",
               })}
             </p>
-            <h4 data-h2-font-size="b(h4)" data-h2-margin="b(bottom, none)">
-              {intl.formatMessage(
-                {
-                  defaultMessage: "Employees with shared passports",
-                },
-                { bold },
-              )}
-            </h4>
-
+            <h2 data-h2-font-size="b(h4)" data-h2-margin="b(top, none)">
+              {intl.formatMessage({
+                defaultMessage: "Employees with shared passports",
+              })}
+            </h2>
             <PassportCard
               title={intl.formatMessage({
                 defaultMessage: "Frank Turot",
@@ -89,53 +85,58 @@ const ManagerDashboard: React.FunctionComponent = () => {
                 </p>
               </li>
             </ul>
-            <h4 data-h2-font-size="b(h4)" data-h2-margin="b(all, none)">
+            <h2 data-h2-font-size="b(h4)" data-h2-margin="b(all, none)">
               {intl.formatMessage({
                 defaultMessage: "Contact a Case Manager/DTA Officer",
               })}
-            </h4>
+            </h2>
             <p data-h2-margin="b(bottom, m)">
               {intl.formatMessage({
                 defaultMessage:
                   "Activate the link to contact a workplace accommodation professional in your organization.",
               })}
             </p>
-
-            <Button
-              type="submit"
-              color="blue"
-              mode="solid"
-              data-h2-font-style="b(underline)"
-              data-h2-padding="b(all, s)"
-              data-h2-margin="b(bottom, m)"
-            >
-              {intl.formatMessage({
-                defaultMessage: "Get in touch",
-              })}
-            </Button>
-            <h4 data-h2-font-size="b(h4)" data-h2-margin="b(all, none)">
+            <Link href="#">
+              <a
+                {...colorMap.blue.solid}
+                data-h2-padding="b(all, s)"
+                data-h2-width="b(100)"
+                data-h2-display="b(block)"
+                data-h2-text-align="b(center)"
+                data-h2-radius="b(s)"
+                data-h2-margin="b(bottom, m)"
+              >
+                {intl.formatMessage({
+                  defaultMessage: "Get in touch",
+                })}
+              </a>
+            </Link>
+            <h2 data-h2-font-size="b(h4)" data-h2-margin="b(all, none)">
               {intl.formatMessage({
                 defaultMessage:
                   "Contact the Accessibility, Accommodation and Adaptive Computer Technology (AAACT) Team",
               })}
-            </h4>
+            </h2>
             <p data-h2-margin="b(bottom, m)">
               {intl.formatMessage({
                 defaultMessage:
                   "Activate the link to contact the AAACT team for an information session or an assessment.",
               })}
             </p>
-            <Button
-              type="submit"
-              color="blue"
-              mode="solid"
-              data-h2-font-style="b(underline)"
-              data-h2-padding="b(all, s)"
-            >
-              {intl.formatMessage({
-                defaultMessage: "Contact the AAACT",
-              })}
-            </Button>
+            <Link href="https://www.canada.ca/en/shared-services/corporate/aaact-program.html">
+              <a
+                {...colorMap.blue.solid}
+                data-h2-padding="b(all, s)"
+                data-h2-width="b(100)"
+                data-h2-display="b(block)"
+                data-h2-text-align="b(center)"
+                data-h2-radius="b(s)"
+              >
+                {intl.formatMessage({
+                  defaultMessage: "Contact the AAACT",
+                })}
+              </a>
+            </Link>
           </div>
         </RightSection>
       </Page>
