afb29f9245d675cda7e58af54a5dac8647d29617
Yoni K <yoni.kid15@gmail.com>
2022-05-16 11:30:43 -0400

    Refactor register layout/styling

--- pages/register.tsx
+++ pages/register.tsx
@@ -5,7 +5,6 @@ import Button from "../components/Button";
 import { Input } from "../components/formComponents";
 import FormFooter from "../components/FormFooter";
 import Layout from "../components/Layout";
-import Page from "../components/Page";
 import SelectRoleDialog from "../components/SelectRoleDialog";
 import fetchJson, { FetchError } from "../lib/fetchJson";
 import useUser from "../lib/useUser";
@@ -33,7 +32,7 @@ const Register: React.FunctionComponent = () => {
   const { handleSubmit } = methods;
   // here we just check if user is already logged in and redirect to profile
   const { mutateUser } = useUser({
-    redirectTo: "/passport",
+    redirectTo: `${user.manager ? "/manager/manager-dashboard" : "/passport"}`,
     redirectIfFound: true,
   });
   const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
@@ -47,7 +46,7 @@ const Register: React.FunctionComponent = () => {
 
   const handleLogin = async (userData: User): Promise<void> => {
     const body = {
-      name: `${userData.name}`,
+      name: "Frank", // Replace with user
     };
 
     try {
@@ -100,124 +99,120 @@ const Register: React.FunctionComponent = () => {
       headTitle={intl.formatMessage({
         defaultMessage: "Register - GC Workplace Accessibility Passport",
       })}
+      formLayout
+      data-h2-width="b(75) s(50)"
+      data-h2-padding="b(top-bottom, l) b(right-left, m) m(right-left, xl)"
     >
-      <Page
-        data-h2-justify-content="b(center)"
-        data-h2-flex-direction="b(column)"
-      >
-        <div data-h2-width="s(50)" style={{ margin: "auto" }}>
-          <FormProvider {...methods}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div data-h2-margin="b(bottom, l)">
-                <div data-h2-display="b(flex)" style={{ gap: "1rem" }}>
-                  <div style={{ flex: "1 " }}>
-                    <Input
-                      id="firstName"
-                      name="firstName"
-                      type="text"
-                      label={intl.formatMessage({
-                        defaultMessage: "Given name(s)",
-                      })}
-                      rules={{
-                        required: intl.formatMessage(errorMessages.required),
-                      }}
-                    />
-                  </div>
-                  <div style={{ flex: "1 " }}>
-                    <Input
-                      id="lastName"
-                      name="lastName"
-                      type="text"
-                      label={intl.formatMessage({
-                        defaultMessage: "Surname(s)",
-                      })}
-                      rules={{
-                        required: intl.formatMessage(errorMessages.required),
-                      }}
-                    />
-                  </div>
-                </div>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div data-h2-margin="b(bottom, l)">
+            <div data-h2-display="b(flex)" style={{ gap: "1rem" }}>
+              <div style={{ flex: "1 " }}>
                 <Input
-                  id="email"
-                  name="email"
+                  id="firstName"
+                  name="firstName"
                   type="text"
                   label={intl.formatMessage({
-                    defaultMessage: "Email address",
-                  })}
-                  rules={{
-                    required: intl.formatMessage(errorMessages.required),
-                  }}
-                />
-                <p
-                  data-h2-font-size="b(caption)"
-                  data-h2-margin="b(top, s) b(bottom, none)"
-                >
-                  {intl.formatMessage({
-                    defaultMessage:
-                      "Please create a password. Passwords must contain at least:",
-                  })}
-                </p>
-                <ul
-                  data-h2-margin="b(bottom-left, s)"
-                  style={{ listStyleType: "disc" }}
-                >
-                  {passwordHints.map((hint) => (
-                    <li key={hint} style={{ marginTop: "0" }}>
-                      <p
-                        data-h2-font-size="b(caption)"
-                        data-h2-margin="b(all, none)"
-                      >
-                        {hint}
-                      </p>
-                    </li>
-                  ))}
-                </ul>
-                <Input
-                  id="password"
-                  name="password"
-                  type="password"
-                  label={intl.formatMessage({ defaultMessage: "Password" })}
-                  rules={{
-                    required: intl.formatMessage(errorMessages.required),
-                  }}
-                />
-                <Input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                  label={intl.formatMessage({
-                    defaultMessage: "Confirm Password",
+                    defaultMessage: "Given name(s)",
                   })}
                   rules={{
                     required: intl.formatMessage(errorMessages.required),
                   }}
                 />
               </div>
-              <FormFooter
-                cancelButton={{
-                  href: "/",
-                  title: intl.formatMessage({
-                    defaultMessage: "Already have an account? Log in instead.",
-                  }),
-                }}
-              >
-                <Button
-                  type="submit"
-                  color="blue"
-                  mode="solid"
-                  data-h2-font-style="b(underline)"
-                  data-h2-padding="b(all, s)"
-                >
-                  {intl.formatMessage({
-                    defaultMessage: "Register",
+              <div style={{ flex: "1 " }}>
+                <Input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  label={intl.formatMessage({
+                    defaultMessage: "Surname(s)",
                   })}
-                </Button>
-              </FormFooter>
-            </form>
-          </FormProvider>
-        </div>
-        <SelectRoleDialog isOpen={isOpen} handleSelectRole={handleSelectRole} />
-      </Page>
+                  rules={{
+                    required: intl.formatMessage(errorMessages.required),
+                  }}
+                />
+              </div>
+            </div>
+            <Input
+              id="email"
+              name="email"
+              type="text"
+              label={intl.formatMessage({
+                defaultMessage: "Email address",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <p
+              data-h2-font-size="b(caption)"
+              data-h2-margin="b(top, s) b(bottom, none)"
+            >
+              {intl.formatMessage({
+                defaultMessage:
+                  "Please create a password. Passwords must contain at least:",
+              })}
+            </p>
+            <ul
+              data-h2-margin="b(bottom-left, s)"
+              style={{ listStyleType: "disc" }}
+            >
+              {passwordHints.map((hint) => (
+                <li key={hint} style={{ marginTop: "0" }}>
+                  <p
+                    data-h2-font-size="b(caption)"
+                    data-h2-margin="b(all, none)"
+                  >
+                    {hint}
+                  </p>
+                </li>
+              ))}
+            </ul>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              label={intl.formatMessage({ defaultMessage: "Password" })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+            <Input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              label={intl.formatMessage({
+                defaultMessage: "Confirm Password",
+              })}
+              rules={{
+                required: intl.formatMessage(errorMessages.required),
+              }}
+            />
+          </div>
+          <FormFooter
+            cancelButton={{
+              href: "/",
+              title: intl.formatMessage({
+                defaultMessage: "Already have an account? Log in instead.",
+              }),
+            }}
+          >
+            <Button
+              type="submit"
+              color="blue"
+              mode="solid"
+              data-h2-font-style="b(underline)"
+              data-h2-padding="b(all, s)"
+            >
+              {intl.formatMessage({
+                defaultMessage: "Register",
+              })}
+            </Button>
+          </FormFooter>
+        </form>
+      </FormProvider>
+      <SelectRoleDialog isOpen={isOpen} handleSelectRole={handleSelectRole} />
     </Layout>
   );
 };
