e8cfbe0cc495d62e9c815834b21a8767cd54ffff
Yoni K <yoni.kid15@gmail.com>
2022-05-16 21:18:53 -0400

    Refactor messages function on _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import "../styles/hydrogen.css";
-import English from "../lang/en.json";
 import French from "../lang/fr.json";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
@@ -13,14 +12,7 @@ function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
   const { locale = "en" } = useRouter();
 
   const messages = useMemo(() => {
-    switch (locale) {
-      case "en":
-        return English;
-      case "fr":
-        return French;
-      default:
-        return English;
-    }
+    return locale === "fr" ? French : undefined;
   }, [locale]);
 
   const { pathname } = useRouter();
