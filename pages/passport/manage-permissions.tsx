5fe70209fca50d0e1599bbb3b295acee4d077e32
Matt Nigh <matt@mattnigh.com>
2022-05-27 14:57:52 -0400

    Remove intl.formatMessage from strings not requiring translation (proper names)

--- pages/passport/manage-permissions.tsx
+++ pages/passport/manage-permissions.tsx
@@ -59,9 +59,7 @@ const ManagePermissions: React.FunctionComponent = () => {
             </h3>
             <div data-h2-margin="b(bottom, l)">
               <PermissionsCard
-                title={intl.formatMessage({
-                  defaultMessage: "Sui Kiyoko",
-                })}
+                title="Sui Kiyoko"
                 actionLinks={[
                   intl.formatMessage({
                     defaultMessage: "Remove all access",
@@ -82,9 +80,7 @@ const ManagePermissions: React.FunctionComponent = () => {
               })}
             </h3>
             <PermissionsCard
-              title={intl.formatMessage({
-                defaultMessage: "Priyanka Luka",
-              })}
+              title="Priyanka Luka"
               actionLinks={[
                 intl.formatMessage({
                   defaultMessage: "Remove all access",
