cec672a5dc248af298615aa183a1863d31bdf431
Matt Nigh <matt@mattnigh.com>
2022-05-31 11:36:01 -0400

    Make PassportCard component responsive

--- components/PassportCard.tsx
+++ components/PassportCard.tsx
@@ -13,7 +13,7 @@ export const PassportCard: React.FunctionComponent<{
       data-h2-margin="b(top, s)"
       data-h2-shadow="b(m)"
       data-h2-padding="b(all, m)"
-      data-h2-display="b(flex)"
+      data-h2-display="b(block) s(flex)"
       data-h2-justify-content="b(space-between)"
       data-h2-align-items="b(center)"
     >
